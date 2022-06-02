2cc82601244afb98413e2ade6faa7845bedb5c61
Ваше Имя <you@example.com>
2022-06-02 13:09:30 +0600

    first day

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        Product List
+      </main>
+    </div>
+  );
+}
+
+export default App;
